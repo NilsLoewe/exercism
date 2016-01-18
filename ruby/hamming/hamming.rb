fa7bd8ec0e7e734278012d4afb272fcf085c0d47
Nils Löwe <nils.loewe@wlw.de>
2016-01-18 13:55:23 +0100

    ruby e02

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,6 +4,10 @@ class Hamming
   def self.compute(dna1, dna2)
     raise ArgumentError.new("Sequences must have the same length") unless dna1.length == dna2.length
 
-    (dna1.chars - dna2.chars).length
+    diffs = 0
+    dna1.chars.each_with_index do |c, i|
+      diffs +=1 if dna2.chars[i] != c
+    end
+    diffs
   end
 end
