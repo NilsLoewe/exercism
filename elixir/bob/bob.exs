b8056ea89c2c4653006db8751ae3a0d1b9501c5d
Nils Löwe <nils.loewe@wlw.de>
2016-02-11 09:03:22 +0100

    Simplify even more

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,18 +3,8 @@ defmodule Bob do
     cond do
       String.ends_with?(input, "?") == true -> "Sure."
       String.strip(input) == ""             -> "Fine. Be that way!"
-      check_upcase(input)                   -> "Whoa, chill out!"
+      Regex.match?( ~r/[\p{L}]/, input) && String.upcase(input) == input -> "Whoa, chill out!"
       input                                 -> "Whatever."
-    end
-  end
-
-  def check_upcase(input) do
-    if Regex.match?( ~r/[A-Z]/, input) ||
-       Regex.match?( ~r/[\p{L}]/, input)
-       do
-        String.upcase(input) == input
-    else
-        false
     end
   end
 end
