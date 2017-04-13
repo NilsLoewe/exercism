fb34c2a0ae517426086ad2645c33f179766bb018
Nils Löwe <nils@loewe.io>
2017-04-13 10:26:37 +0200

    Ruby 10: Solution without extra module

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,41 +1,29 @@
 # Exercism Ruby path: RobotName exercise
 class Robot
   attr_reader :name
+  @@names = []
 
   def initialize
     @name = get_new_name
   end
 
   def reset
-    @name = random_name
+    @name = get_new_name
   end
 
   private
 
   def get_new_name
-    name = random_name
-    while RobotNames.name_taken?(name)
+    begin
       name = random_name
-    end
-    RobotNames.use_name(name)
+    end while @@names.include?(name)
+    @@names << name
     name
   end
 
   def random_name
-    name = ('A'..'Z').to_a.sample(2).join
+    name = ('AA'..'ZZ').to_a.sample
     name << (0..9).to_a.sample(3).join.to_s
-  end
-end
-
-module RobotNames
-  @names = []
-
-  def self.name_taken?(name)
-    @names.include?(name)
-  end
-
-  def self.use_name(name)
-    @names << name
   end
 end
 
